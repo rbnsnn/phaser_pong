91fe7eb7e34479f9fa4785880a0a64a488172871
Milosz <milosz.mokrzycki21@gmail.com>
2021-05-23 13:34:52 +0200

    organizacja kodu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+//Window Width and Height
+
 const GW = 1200;
 const GH = 700;
 
-// const ball = {
-//     size: 20,
-//     speed: 100,
-// }
+//Phaser init
+
 let config = {
     type: Phaser.AUTO,
     width: GW,
@@ -24,6 +24,9 @@ let config = {
         update: update
     }
 };
+
+//Variables
+
 let game = new Phaser.Game(config);
 let ball;
 let paddlePlayer;
@@ -31,25 +34,35 @@ let paddleAi;
 let cursor;
 
 function preload() {
+    //Preloading assets
+
     this.load.image('ball', '../assets/ball.png')
     this.load.image('paddle', '../assets/paddle.png')
     this.load.image('smalec', '../assets/smalectrailbig.png');
 }
 
 function create() {
+    //Create keyboard input
+
     cursors = this.input.keyboard.createCursorKeys();
+
+    //Player paddle set
+
     paddlePlayer = this.physics.add.image(100, (GH / 2), 'paddle');
     paddlePlayer.setImmovable(true);
     paddlePlayer.setCollideWorldBounds(true);
     paddlePlayer.setBounce(1);
     paddlePlayer.setDepth(5);
 
+    //AI paddle set
 
     paddleAi = this.physics.add.image((GW - 100), (GH / 2), 'paddle');
     paddleAi.setImmovable(true);
     paddleAi.setCollideWorldBounds(true);
     paddleAi.setBounce(1);
     paddleAi.setDepth(5);
+
+    //Ball set
 
     let vecX = Phaser.Math.Between(100, 300);
     let vecY = Phaser.Math.Between(-300, 300);
@@ -59,12 +72,14 @@ function create() {
     ball.setVelocity(vecX, vecY);
     ball.setDepth(5);
 
+    //Moving paddle via mouse
+
     this.input.on('pointermove', function (pointer) {
         paddlePlayer.y = pointer.y;
     }, this);
 
 
-
+    //Particle emmiter
 
     let particles = this.add.particles('smalec');
 
@@ -80,53 +95,47 @@ function create() {
     emitter.setScale(1);
     emitter.startFollow(ball);
 
-
+    //Ball bounce from Player paddle
 
     const hitPaddlePlayer = function () {
         let diff = 0;
 
         if (ball.y < paddlePlayer.y) {
-            //  Ball is on the left-hand side of the paddle
             diff = paddlePlayer.y - ball.y;
             ball.setVelocityY(-2 * diff);
         } else if (ball.y > paddlePlayer.y) {
-            //  Ball is on the right-hand side of the paddle
             diff = ball.y - paddlePlayer.y;
             ball.setVelocityY(2 * diff);
         } else {
-            //  Ball is perfectly in the middle
-            //  Add a little random X to stop it bouncing straight up!
             ball.setVelocityY(2 + Math.random() * 8);
         }
     }
+
+    //Ball bounce from AI paddle
 
     const hitPaddleAi = function () {
         let diff = 0;
 
         if (ball.y < paddleAi.y) {
-            //  Ball is on the left-hand side of the paddle
             diff = paddleAi.y - ball.y;
             ball.setVelocityY(-2 * diff);
         } else if (ball.y > paddleAi.y) {
-            //  Ball is on the right-hand side of the paddle
             diff = ball.y - paddleAi.y;
             ball.setVelocityY(2 * diff);
         } else {
-            //  Ball is perfectly in the middle
-            //  Add a little random X to stop it bouncing straight up!
             ball.setVelocityY(2 + Math.random() * 8);
         }
     }
 
+    //Collisions
+
     this.physics.add.collider(ball, paddlePlayer, hitPaddlePlayer, null, this);
     this.physics.add.collider(ball, paddleAi, hitPaddleAi, null, this);
-
-
-
-
 }
 
 function update() {
+    //Reseting ball after hitting score
+
     const ballReset = function () {
         if (ball.x <= ball.width / 2 || ball.x >= (GW - ball.width / 2)) {
             ball.x = GW / 2;
@@ -134,7 +143,7 @@ function update() {
         }
     }
 
-
+    //Keyboard input for paddle move
 
     paddleAi.setVelocity(0);
 
